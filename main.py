3d5d3493899fe5dab2fb21327319a9c6bec963b5
Gsamsaj <s.jimenez@udea.edu.co>
2026-09-01 22:37:40 -0500

    Executing different analysis

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 from Xrunner_no_pandas import Xruner
 
-airfoil = "airfoils\\HSNLF(1)-0213.txt"
-naca_code = "2412"
-Rey = 1e6
+airfoil = "airfoils\\SSC-A09.txt"
+naca_code = "4412"
+Rey = 2.5e6
 
-alpha = 6
-AOA_seq = [-2, 17, 0.5]
+alpha = 3
+AOA_seq = [-5, 18, 1]
 
-panels = [100, 1000, 50]
+panels = [100, 494, 20]
 
-plot_datas = [("alpha","CL"), ("alpha","CD"), ("alpha","L/D"), ("alpha","CM")]
+plot_datas = [("alpha","CL"), ("alpha","CD"), ("alpha","L/D"), ("alpha","CM"), ("CD","CL")]
 plot_conv = "CL"
 
-juanito = Xruner(airfoil=airfoil, Re=Rey)
+puta = Xruner(naca=naca_code, Re=2.5e6, pan=400, verbose=False)
 
 #juanito.run_alpha(AOA = alpha,
 #                  filename = 'hola.txt',
 #                  dumpfile = "juanitodump.txt",
 #                  airfoil_plot = True)
 
-#juanito.run_aseq(aseq = AOA_seq, 
-#                 filename= "seq_alphas.txt", 
-#                 plot_data = plot_datas)
+puta.run_aseq(aseq = AOA_seq, 
+                 filename= "seq_alphas.txt", 
+                 plot_data = plot_datas)
 
-juanito.run_mesh_conv(AOA = alpha,
-                      pan_range = panels,
-                      filename = "mesh_conv.txt",
-                      plot_data = plot_conv)
+#juanito.run_mesh_conv(AOA = alpha,
+#                      pan_range = panels,
+#                      filename = "mesh_conv.txt",
+#                      plot_data = plot_conv)
