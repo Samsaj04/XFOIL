a8fffbc5a1436d14064b8231cd8e0f080ab95262
Gsamsaj <s.jimenez@udea.edu.co>
2026-09-13 01:21:45 -0500

    Testing

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from Xrunner_OOP import Xruner
+from Xrunner_OOP_V2 import Xruner
+from extra.xfoil_plots import plot_polar_dump, plot_airfoil
 
 airfoil = "holaputa_opti.txt"
 Rey = 1e6
 
 alpha = 1
 
-plot_datas = [("alpha","CL"), ("alpha","CD"), ("alpha","L/D"), ("alpha","CM"), ("CD","CL")]
-plot_conv = "CL"
+#plot_polars = [("alpha","Cl"), ("alpha","Cd"), ("alpha","L/D"), ("alpha","Cm"), ("Cd","Cl")]
+#plot_dump = [('x', 'Cp'), ('x', 'Ue/Vinf')]
 
-puta = Xruner(naca='2412', Re=Rey, ite=200, pan=160, verbose=True)
-puta.run_alpha(AOA=alpha, filename='hola.txt', dumpfile='juan.txt', airfoil_plot=True)
+puta = Xruner(naca='2412', Re=Rey, ite=200, pan=160, verbose=False)
+
+#polars = puta.run_aseq(aseq=[-3, 12, 1], filename='hola.txt')
+#plot_polar_dump(var=plot_polars, data=polars)
+pol, dump = puta.run_alpha(AOA=alpha, filename='hola.txt', dumpfile='juanito.txt')
+plot_polar_dump(var='dump', data=dump)
